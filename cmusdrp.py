15c59acf1f6bc6e3532ae1e9547ceaccd44d92e0
MacDesu <32233686+macdesu@users.noreply.github.com>
2019-05-05 04:41:51 +0200

    added duration in seconds

--- cmusdrp.py
+++ cmusdrp.py
@@ -12,8 +12,11 @@ def discord():
 		time.sleep(1)
 		artist = os.popen("cmus-remote -Q | grep \"tag artist\" | sed s/\"tag artist \"//").read()
 		title = os.popen("cmus-remote -Q | grep \"tag title\" | sed s/\"tag title \"//").read()
+		duration = os.popen("cmus-remote -Q | grep \"duration\" | sed s/\"duration \"//").read()
+		position = os.popen("cmus-remote -Q | grep \"position\" | sed s/\"position \"//").read()
+		current_time = " (" + position + "/" + duration + ")"
 
-		RPC.update(details=artist, state=title, large_image="logo")
+		RPC.update(details=artist, state=title + current_time, large_image="logo")
 
 if __name__ == "__main__":
 	discord()
